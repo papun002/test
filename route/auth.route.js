b4bdad6ff2ede70e1b7e9ba413d2843342c3dcf8
Rohit Kumar Rana <105841722+papun002@users.noreply.github.com>
2025-12-09 19:43:28 +0530

    Update auth.route.js

--- route/auth.route.js
+++ route/auth.route.js
@@ -1,23 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authController = require('../controllers/auth-controller/auth-controller');
+const authController = require("../controllers/auth-controller/auth-controller");
+const getCid = require("../middleware/getCid-middlware/getCid");
 
 // Public Routes
-router.post('/auth/signup', authController.signup);
-router.post('/auth/login', authController.login);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+router.post("/auth/signup", authController.signup);
+router.post("/auth/login", authController.login);
+router.post("/staff/login", authController.conductorLogin);
 
 module.exports = router;
