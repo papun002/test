7f392887b402388505d006ed7783817fe814ba77
Rohit Kumar Rana <105841722+papun002@users.noreply.github.com>
2025-12-12 15:48:51 +0530

    Add edit route functionality to route vehicle

--- route/route-vehicle/route.vehicle.js
+++ route/route-vehicle/route.vehicle.js
@@ -13,13 +13,12 @@ router.post("/create/route", getCid, routeController.createRoute);
 // Get all routes
 router.get("/get/routes", getCid, routeController.getRoutes);
 
+router.put("/edit/routes", getCid, routeController.editRoute);
+
 // Delete a route
 router.delete("/delete/route", getCid, routeController.deleteRoute);
-
-
 
 // -----------------------------conductorModule--------------------------------
 router.get("/get/staff/routes", getSidOfStaff, routeController.getRoutes);
 
 module.exports = router;
-
