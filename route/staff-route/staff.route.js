f5814d983425a5b05db6c5cfa885eeab3f48c0ed
papun002 <papun002@gmail.com>
2025-12-13 15:00:17 +0530

    staff name for cond modu

--- route/staff-route/staff.route.js
+++ route/staff-route/staff.route.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { getCid } = require("../../middleware/getCid-middlware/getCid");
 const staffController = require("../../controllers/saff-controller/staff.controller");
+const { getSidOfStaff } = require("../../middleware/getCid-middlware/getSidofStaff");
 
 // Create a new staff member (driver or conductor)
 router.post("/create/staff", getCid, staffController.createStaff);
@@ -13,5 +14,11 @@ router.get("/getstaffbymobile", getCid, staffController.getStaffByMobile);
 
 
 router.get("/get/driver/allsummary", getCid,staffController.getStaffAllSummary)
+
+
+// for conductor module
+
+router.get("/get/staff/staff",getSidOfStaff, staffController.getStaff);
+
 
 module.exports = router;
