7bfd78f281e8d61d1dd6ce9dc67034c562c7157f
papun002 <papun002@gmail.com>
2026-01-08 18:25:42 +0530

    role

--- controllers/auth-controller/auth-controller.js
+++ controllers/auth-controller/auth-controller.js
@@ -65,7 +65,7 @@ exports.login = async (req, res) => {
 
     return res
       .status(200)
-      .json({ message: "Login successful", firm: firmData.firmName, name: firmData.name, token, role: firmData.role });
+      .json({ message: "Login successful", firm: firmData.firmName, name: firmData.name, token, role: "owner" });
   } catch (error) {
     console.error("Error during login:", error);
     return res.status(500).json({ message: "Internal server error" });
